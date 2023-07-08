a10a1f54716378b60fd23919da64789afa40411d
Stuyk <stuykgaming@gmail.com>
2023-07-07 21:49:38 -0600

    Add New Types

--- types/server.d.ts
+++ types/server.d.ts
@@ -2,8 +2,7 @@ import * as alt from 'alt-server';
 import { Account } from '../shared/interfaces';
 
 declare module 'alt-server' {
-    export function on<T = Account>(
-        eventName: 'crc-discord-login-finish',
-        listener: (player: alt.Player, account: T) => void
-    ): void;
+    interface ICustomEmitEvent {
+        'crc-discord-login-finish': <T = Account>(player: alt.Player, account: T) => void;
+    }
 }
